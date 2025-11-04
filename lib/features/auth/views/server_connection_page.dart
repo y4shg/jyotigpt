c5f793b93523dff58497ce1c2db893a278209f19
Yash Ghule <fpsghuley77@stu.fpsk12.net>
2025-11-04 02:09:03 +0000

    lol forgot to add images, these are very silly commit descriptions

--- lib/features/auth/views/server_connection_page.dart
+++ lib/features/auth/views/server_connection_page.dart
@@ -164,7 +164,7 @@ class _ServerConnectionPageState extends ConsumerState<ServerConnectionPage> {
         body: Stack(
           children: [
             Positioned.fill(
-              child: Image.asset('assets/images/background.jpg', fit: BoxFit.cover),
+              child: Image.asset('assets/images/background.png', fit: BoxFit.cover),
             ),
             Positioned.fill(
               child: Container(color: Colors.black.withOpacity(0.3)),
