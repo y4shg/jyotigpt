01832a2c0b9a3515bd123599c2e299cd70caa57d
Yash Ghule <y.ghule77@gmail.com>
2026-01-27 00:28:30 +0000

    Adjust ChatPlaceholder font size and update Audio icon stroke width in MessageInput

--- src/lib/components/chat/ChatPlaceholder.svelte
+++ src/lib/components/chat/ChatPlaceholder.svelte
@@ -79,7 +79,7 @@
 		{/if}
 
 		<div
-			class=" mt-2 mb-4 text-3xl text-gray-800 dark:text-gray-100 text-left flex items-center gap-4 font-primary"
+			class=" mt-2 mb-4 text-2xl text-gray-800 dark:text-gray-100 text-left flex items-center gap-4 font-primary"
 		>
 			<div>
 				<div class=" capitalize line-clamp-1" in:fade={{ duration: 200 }}>
